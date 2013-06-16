6f3f2107eb5c635bc261bb4303482438b06d0d95
IIUJ-KrzysztofDawidowicz <krzysztof.dawidowicz@ii.uj.edu.pl>
2013-06-16 23:21:11 +0200

    wydzielono klasę Camera do osobnego pliku

--- Projekt/camera/ControlsGLUT.h
+++ Projekt/camera/ControlsGLUT.h
@@ -1,4 +1,4 @@
-
+#pragma once
 #include "GLee.h"                  // GL Easy Extension library
 #include <gl/gl.h>                 // standardowy plik nag³ówkowy OpenGL
 #include <gl/glu.h>                // plik nag³ówkowy dodatkowych aplikacji bibliotek OpenGL
