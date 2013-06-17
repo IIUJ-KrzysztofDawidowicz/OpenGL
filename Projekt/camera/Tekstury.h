6f7595f329b4a1ae4956c51554696b312fb1079c
IIUJ-KrzysztofDawidowicz <krzysztof.dawidowicz@ii.uj.edu.pl>
2013-06-17 12:00:55 +0200

    class Tekstury moved out

--- Projekt/camera/Tekstury.h
+++ Projekt/camera/Tekstury.h
@@ -1,3 +1,18 @@
 #pragma once
+#include "GLee.h"                  // GL Easy Extension library
+#include <gl/gl.h>                 // standardowy plik nag³ówkowy OpenGL
+#include <gl/glu.h>                // plik nag³ówkowy dodatkowych aplikacji bibliotek OpenGL
+class Tekstury
+{
+	static int LoadGLTexture(char *plik);
+public:
+	GLuint tekstura;
+	Tekstury(void);
+	Tekstury(char* plik)
+	{
+		tekstura = LoadGLTexture(plik);
+	}
+	~Tekstury(void);
 
+};
 
--- Projekt/camera/main.cpp
+++ Projekt/camera/main.cpp
@@ -46,72 +46,15 @@ int /*screenWidth, screenHeight,*//* mouseX, mouseY,*/ board;
 
 
 
-class Tekstury
-{
-public:
-	GLuint tekstura;
-	Tekstury(void);
-	Tekstury(char* plik)
-	{
-		tekstura = LoadGLTexture(plik);
-	}
-	~Tekstury(void);
 
-	static int LoadGLTexture(char *plik);
-};
-Tekstury::Tekstury(void)
-{
-}
-
-
-Tekstury::~Tekstury(void)
-{
-}
-int Tekstury::LoadGLTexture(char *plik)
-{
-  unsigned int Texture;
-  FILE* img = NULL;
-
-  img = fopen(plik,"rb");
-  if (img==NULL)
-  {
-	MessageBox(NULL, "Nie mo¿na otworzyæ pliku z tekstur¹", "B³¹d", MB_OK);
-	return -1;
-  }
-  unsigned long bWidth = 0;	
-  unsigned long bHeight = 0;	
-  DWORD size = 0;	
-
-  fseek(img,18,SEEK_SET);
-  fread(&bWidth,4,1,img);
-  fread(&bHeight,4,1,img);
-  fseek(img,0,SEEK_END);
-  size = ftell(img) - 54;
-
-  unsigned char *data = (unsigned char*)malloc(size);
-  if (data==NULL)
-  {
-	MessageBox(NULL, "Nie mo¿na zaalokowaæ pamiêci na teksturê", "B³¹d", MB_OK);
-	return -1;
-  }
-
-  fseek(img,54,SEEK_SET);	// image data
-  fread(data,size,1,img);
-  fclose(img);
-
-  glGenTextures(1, &Texture);
-  glBindTexture(GL_TEXTURE_2D, Texture);
-  gluBuild2DMipmaps(GL_TEXTURE_2D, 3, bWidth, bHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, data);
-
-  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-  
-  free(data);
-  return Texture;
-}
 
 Tekstury tekstury[3];
 
+
+ControlsGLUT* controls = new ControlsGLUT();
+
+
+Camera camera = Camera(controls, &screenWidth, &screenHeight);
 
 //GLuint tekstura[3];
 
@@ -164,11 +107,6 @@ void msgbox(const char *msg,char *title){
 	MessageBoxA(NULL,msg,title,MB_OKCANCEL);
 }
 
-
-ControlsGLUT* controls = new ControlsGLUT();
-
-
-Camera camera = Camera(controls, &screenWidth, &screenHeight);
 
 // get mouse button state
 void processMouse(int button, int state, int x, int y) {
